b885b8fc3ae42a94fed0fb0006cd39fac76d4372
New Future <new_future@msn.cn>
2016-06-12 15:02:09 +0800

    添加公网IP查询接口

--- ip.py
+++ ip.py
@@ -3,22 +3,40 @@
 
 import socket
 import urllib2
-DEBUG = False
+DEBUG = False  # 是否打印错误
 
 
 def local_v6(i=0):  # 本地ipv6地址
     info = socket.getaddrinfo(socket.gethostname(), 0, socket.AF_INET6)
-    if DEBUG: print(info)
+    if DEBUG:
+        print(info)
     return info[i][4][0]
 
 
 def local_v4(i=0):  # 本地ipv4地址
     info = socket.getaddrinfo(socket.gethostname(), 0, socket.AF_INET)
-    if DEBUG: print(info)
+    if DEBUG:
+        print(info)
     return info[i][-1][0]
 
 
-def nku(): #nku 内网ipv4地址
+def public_v4(url="http://v4.ipv6-test.com/api/myip.php"):  # 公网IPV4地址
+    try:
+        return urllib2.urlopen(url, timeout=60).read()
+    except Exception, e:
+        if DEBUG:
+            print(e)
+
+
+def public_v6(url="http://v6.ipv6-test.com/api/myip.php"):  # 公网IPV6地址
+    try:
+        return urllib2.urlopen(url, timeout=60).read()
+    except Exception, e:
+        if DEBUG:
+            print(e)
+
+
+def nku():  # nku 内网ipv4地址
     host = ['http://202.113.18.110', 'http://202.113.18.210']
     for url in host:
         try:
@@ -30,5 +48,6 @@ def nku(): #nku 内网ipv4地址
                 if result != None:
                     return result
         except Exception, e:
-            if DEBUG: print e
+            if DEBUG:
+                print(p)
             continue
