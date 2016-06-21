af1c7f406315ee2b53671ac84661ad3197837857
New Future <new_future@msn.cn>
2016-06-21 17:42:10 +0800

    change config

--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 import dnspod
 import ip
 import json
-
+import argparse
 
 def get_config(key=None, file="config.json"):
     if not hasattr(get_config, "config"):
@@ -20,10 +20,11 @@ def get_config(key=None, file="config.json"):
 
 
 def update():
-    if get_config('index4') == 'nku':
-        ipv4 = ip.nku()
+    index4=get_config('index4') or 0
+    if str(index4).isdigit():
+        ipv4 = ip.local_v4(index4)
     else:
-        ipv4 = ip.local_v4(get_config('index4'))
+        ipv4 = getattr(ip,index4+"_v4")()
     print 'update ipv4 to:', ipv4
     if ipv4 != None:
         for domain in get_config('ipv4'):
@@ -31,13 +32,20 @@ def update():
 
     v6_domains=get_config("ipv6")
     if len(v6_domains) > 0:
-        ipv6 = ip.local_v6(get_config('index6'))
+        index6=get_config('index6')
+        if str(index6).isdigit():
+            ipv6 = ip.local_v6(index6)
+        else:
+            ipv6 = getattr(ip,index6+"_v6")()
         print 'update ipv6 to:', ipv6
         if ipv6 != None:
             for domain in v6_domains:
                 print dnspod.change_record(domain, ipv6, 'AAAA')
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c',default="config.json")
+    get_config(file=parser.parse_args().c)
     dnspod.TOKEN = "%s,%s" % (get_config('id'), get_config('token'))
     dnspod.PROXY = get_config('proxy')
     ip.DEBUG = get_config('debug')
