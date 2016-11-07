542780e3098801fba34ec7f0e80414db22c3f840
NewFuture <github@newfuture.cc>
2016-11-07 12:03:31 +0800

    linux run脚本

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-"$RUN_DIR/run.py" >> "$RUN_DIR/run.log"
+"$RUN_DIR/run.py" -c "$RUN_DIR/config.json" >> "$RUN_DIR/run.log"
