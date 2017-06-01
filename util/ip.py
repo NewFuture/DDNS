71af689cdb06179679e10401550418c49ceb8d0f
NewFuture <github@newfuture.cc>
2017-06-01 23:09:27 +0800

    #4 数字参数字符串格式兼容

--- util/ip.py
+++ util/ip.py
@@ -32,14 +32,14 @@ def local_v6(i=0):  # 本地ipv6地址
     info = socket.getaddrinfo(socket.gethostname(), 0, socket.AF_INET6)
     if DEBUG:
         print(info)
-    return info[i][4][0]
+    return info[int(i)][4][0]
 
 
 def local_v4(i=0):  # 本地ipv4地址
     info = socket.getaddrinfo(socket.gethostname(), 0, socket.AF_INET)
     if DEBUG:
         print(info)
-    return info[i][-1][0]
+    return info[int(i)][-1][0]
 
 
 def public_v4(url="http://v4.ipv6-test.com/api/myip.php"):  # 公网IPV4地址
