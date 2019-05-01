256b22ec3f90b365775f827c9124c879284bff56
New <github@newfuture.cc>
2019-05-01 23:04:44 +0800

    ci(pypi): auto set pypi version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.1',  # Required
+    version='${VERSION}',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
