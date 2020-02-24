e2b62b74f0d8841690a05aa74148d9fdff5bfebb
memorz <chenyanbo@bipt.edu.cn>
2020-02-24 18:48:04 +0800

    删除获取网卡IP部分
    删除获取网卡IP部分

--- util/ip.py
+++ util/ip.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import netifaces as ni
 from re import compile
 from os import name as os_name, popen
 from socket import socket, getaddrinfo, gethostname, AF_INET, AF_INET6, SOCK_DGRAM
@@ -46,17 +45,16 @@ def local_v4(i=0):  # 本地ipv4地址
     debug(info)
     return info[int(i)][-1][0]
 
-def interface_v6(interface=None):  # 本地网卡的ipv6地址
-    debug("open: %s", interface)
-    info = ni.ifaddresses(interface)[ni.AF_INET6][0]['addr']
-    return info
-
-
-def interface_v4(interface=None):  # 本地网卡的ipv4地址
-    debug("open: %s", interface)
-    info = ni.ifaddresses()[ni.AF_INET][0]['addr']
+def local_v6(i=0):  # 本地ipv6地址
+    info = getaddrinfo(gethostname(), 0, AF_INET6)
     debug(info)
-    return info
+    return info[int(i)][4][0]
+
+
+def local_v4(i=0):  # 本地ipv4地址
+    info = getaddrinfo(gethostname(), 0, AF_INET)
+    debug(info)
+    return info[int(i)][-1][0]
 
 
 def _open(url, reg):
