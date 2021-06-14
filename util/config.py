9e54d4cca1c7959d5d5bc00d345e8499fbcdfdcb
New Future <NewFuture@users.noreply.github.com>
2021-06-14 12:20:46 +0000

    doc(readme): update config

--- util/config.py
+++ util/config.py
@@ -55,9 +55,9 @@ def init_config(description, doc, version):
 
     __cli_args = parser.parse_args()
     is_configfile_optional = get_config("token") or get_config("id")
-    config_file = get_config("config");
+    config_file = get_config("config")
     if not is_configfile_optional or config_file is not None:
-        __load_config(config_file  or "config.json", is_configfile_optional)
+        __load_config(config_file or "config.json", is_configfile_optional)
         __cli_args.config = config_file or "config.json"
 
 
