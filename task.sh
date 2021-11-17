e66aadd4325208e181066da5517565dc36ea7c4a
ouyangmland <ouyangmland@gmail.com>
2021-11-17 12:24:01 +0800

    Update task.sh

--- task.sh
+++ task.sh
@@ -4,4 +4,4 @@ RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 CMD="\"$RUN_DIR/run.py\" -c \"$RUN_DIR/config.json\" >> \"$RUN_DIR/run.log\""
 
 echo "*/5 * * * *   root    $CMD" > /etc/cron.d/ddns;
-/etc/init.d/cron reload;
+/sbin/service crond reload;
