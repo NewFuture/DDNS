20b85e66441ac4649f64d6bfdb4a5427de0d1b99
New Future <NewFuture@users.noreply.github.com>
2022-07-13 12:30:11 +0000

    fix(dnspod_com): resolve #286

--- dns/dnspod_com.py
+++ dns/dnspod_com.py
@@ -10,4 +10,6 @@ from dns.dnspod import *  # noqa: F403
 
 API.SITE = "api.dnspod.com"  # noqa: F405
 API.DEFAULT = "default"  # noqa: F405
-API.TOKEN_PARAM = "user_token"  # noqa: F405
+
+# https://github.com/NewFuture/DDNS/issues/286
+# API.TOKEN_PARAM = "user_token"  # noqa: F405
