732c5f8c0e5dfd77676d94e47cd55c1653a939cd
char46 <haosiru@h-sr.cn>
2024-04-19 19:15:49 +0800

    修改查询IP的API。 (#436)
    Co-authored-by: New Future <NewFuture@users.noreply.github.com>

--- util/ip.py
+++ util/ip.py
@@ -58,11 +58,11 @@ def _open(url, reg):
         error(e)
 
 
-def public_v4(url="https://myip.ipip.net", reg=IPV4_REG):  # 公网IPV4地址
+def public_v4(url="https://api-ipv4.ip.sb/ip", reg=IPV4_REG):  # 公网IPV4地址
     return _open(url, reg)
 
 
-def public_v6(url="https://myip6.ipip.net", reg=IPV6_REG):  # 公网IPV6地址
+def public_v6(url="https://api-ipv6.ip.sb/ip", reg=IPV6_REG):  # 公网IPV6地址
     return _open(url, reg)
 
 
