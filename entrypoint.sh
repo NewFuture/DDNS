1f13877afc51b1b7833961f73121b831b28ad45f
ZCTMDC <zctmdc@outlook.com>
2025-03-28 14:37:32 +0800

    将运行的逻辑提出到方法中

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,61 +1,64 @@
 #!/usr/bin/env sh
+set -x
 
 # CN: 该脚本用于在容器启动时执行 ddns 命令, 并创建定时任务
 # EN: This script is used to execute the ddns command when the container starts and create a scheduled task
 
-# CN: 如果没有传入参数
-# EN: If no parameters are passed in
-if [ $# -eq 0 ]; then
+# CN: 运行 `ddns` 命令, 在容器启动时执行 `ddns` 命令, 并创建定时任务
+# EN: Run the `ddns` command, execute the `ddns` command when the container starts, and create a cron job
+function  fun_run_and_crond()
+{
+    
     printenv > /etc/environment
-
+    # CN: 如果传入参数包含 `-h` 或 `--help`, 则执行 `ddns -h` 命令
+    # EN: If the parameter contains `-h` or `--help`, execute the `ddns -h` command
+    if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+        exec /ddns -h
+        return
+    fi
     ### CONF FILE OR ENV ###
-    # CN: 不存在 /config 文件夹，则执行
-    # EN: If the /config folder does not exist, execute
+    # CN: 不存在 /config 文件夹，则执行命令
+    # EN: If the /config folder does not exist, execute the command
     if [ ! -d /config ]; then
-        # CN: 不存在 /config.json 文件,则执行 /ddns
-        # EN: If the /config.json file does not exist, execute /ddns
-        if [ ! -f /config.json ]; then
-            exec /ddns
-        fi
-        exec /ddns -c /config.json
-        echo "*/5 * * * *   /ddns -c /config.json" > /etc/crontabs/root
+        exec /ddns $@
+        echo "*/1 * * * *  /ddns $@" > /etc/crontabs/root
         exec crond -f
     fi
     ### CONF FILE OR ENV ###
-
+    
     ### CONF PATH        ###
-    # CN: 存在 /config 文件夹,则遍历 /config 文件夹下的json文件,依次执行 /ddns -c /config/xxx.json
-    # EN: If the /config folder exists, traverse the json file under the /config folder, and execute /ddns -c /config/xxx.json
+    # CN: 存在 /config 文件夹, 且文件夹下有json文件, 则创建shell脚本
+    # EN: If the /config folder exists, and there are json files under the folder, create a shell script
     if [ -d /config ]; then
-      cat >/tmp/run.sh << 'END_OF_SCRIPT'
+    if [ ! "$(ls -A /config/*.json 2>/dev/null)" ]; then
+        ecec /ddns  -c /config/config.json $@
+    fi
+    cat >/tmp/run.sh << 'EOF'
 #!/usr/bin/env sh
+set -x
 # CN: 遍历 /config 文件夹下的json文件,依次执行 /ddns -c /config/xxx.json
 # EN: Traverse the json file under the /config folder, and execute /ddns -c /config/xxx.json
 for file in /config/*.json
 do
-    /ddns -c $file
+    /ddns -c $file $@
 done
-END_OF_SCRIPT
+EOF
         chmod +x /tmp/run.sh
-        # CN: 执行 /tmp/run.sh
-        # EN: Execute /tmp/run.sh
         /tmp/run.sh
-        echo "*/5 * * * *   /tmp/run.sh" > /etc/crontabs/root
+        echo "*/1 * * * *   /tmp/run.sh" > /etc/crontabs/root
         exec crond -f
     fi
     ### CONF PATH        ###
+    
+}
 
+
+if [ $# -eq 0 ]; then
+    fun_run_and_crond
 else
-
     first=`echo $1 | cut -c1`
     if [ "$first" = "-" ]; then
-        exec /ddns $@
-        # CN: 创建定时任务
-        # EN: Create a scheduled task
-        echo "*/5 * * * *   /ddns $@" > /etc/crontabs/root
-        # CN: 执行定时任务
-        # EN: Execute the scheduled task
-        exec crond -f
+        fun_run_and_crond $@
     else
         exec $@
     fi
