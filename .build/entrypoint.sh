d618b78b8a6157fb2e9e80ac471a3ad31e983a04
New Future <NewFuture@users.noreply.github.com>
2025-05-26 18:27:22 +0000

    update build

--- .build/entrypoint.sh
+++ .build/entrypoint.sh
@@ -2,12 +2,12 @@
 
 if [ $# -eq 0 ]; then
   printenv > /etc/environment
-  echo "*/5 * * * *   /ddns -c /config.json" > /etc/crontabs/root
+  echo "*/5 * * * *   /bin/ddns -c /config.json >> /ddns/ddns.log" > /etc/crontabs/root
   exec crond -f
 else
   first=`echo $1 | cut -c1`
   if [ "$first" = "-" ]; then
-    exec /ddns $@
+    exec /bin/ddns $@
   else
     exec $@
   fi
