985ae2dcd9ed567389f90fba09d27f78849e6eaa
New Future <NewFuture@users.noreply.github.com>
2025-05-26 19:34:03 +0000

    取消log 文件

--- .build/entrypoint.sh
+++ .build/entrypoint.sh
@@ -2,8 +2,8 @@
 
 if [ $# -eq 0 ]; then
   printenv > /etc/environment
-  echo "*/5 * * * *  cd /ddns && /bin/ddns >> run.log 2>&1 " > /etc/crontabs/root
-  exec /bin/ddns
+  echo "*/5 * * * *  cd /ddns && /bin/ddns" > /etc/crontabs/root
+  /bin/ddns
   exec crond -f
 else
   first=`echo $1 | cut -c1`
