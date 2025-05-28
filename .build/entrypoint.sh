aac6beffcdae5856e94514740b562941022b14c1
New Future <NewFuture@users.noreply.github.com>
2025-05-28 11:10:00 +0000

    首次失败自动停止

--- .build/entrypoint.sh
+++ .build/entrypoint.sh
@@ -3,9 +3,7 @@
 if [ $# -eq 0 ]; then
   printenv > /etc/environment
   echo "*/5 * * * *  cd /ddns && /bin/ddns" > /etc/crontabs/root
-  /bin/ddns
-  echo "Cron daemon will run every 5 minutes..."
-  exec crond -f
+  /bin/ddns &&  echo "Cron daemon will run every 5 minutes..." && exec crond -f
 else
   first=`echo $1 | cut -c1`
   if [ "$first" = "-" ]; then
