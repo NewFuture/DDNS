9dc2c744b57037a1135ed4091a457f53208061de
New Future <NewFuture@users.noreply.github.com>
2025-06-25 01:14:45 +0800

    update format

--- ddns/provider/print.py
+++ ddns/provider/print.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 """
 PrintProvider
-仅打印出 IP 地址，不进行任何实际 DNS 操作。
+仅打印出 IP 地址，不进行任何实际 DNS 更新
 """
 
 from ._base import BaseProvider
@@ -10,6 +10,7 @@ from ._base import BaseProvider
 class PrintProvider(BaseProvider):
 
     def set_record(self, domain, value, record_type="A", ttl=None, line=None, **extra):
+        self.logger.debug("PrintProvider: %s(%s) => %s", domain, record_type, value)
         ip_type = "IPv4" if record_type == "A" else "IPv6" if record_type == "AAAA" else ""
-        print("{} -- type: {}({}) domain: {}".format(value, record_type, ip_type, domain))
+        print("[{}] {}".format(ip_type, value))
         return True
