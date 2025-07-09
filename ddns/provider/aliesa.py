fd631c7d59ba7950bf97e6d575aa28d059028270
Charles Chin <eallions@gmail.com>
2025-07-10 00:49:00 +0800

    feat: current time as remark for aliesa (#509)
    Signed-off-by: Charles Chin <eallion.s+github@gmail.com>

--- ddns/provider/aliesa.py
+++ ddns/provider/aliesa.py
@@ -7,6 +7,7 @@ AliESA API
 
 from .alidns import AliBaseProvider
 from ._base import join_domain, TYPE_JSON
+from time import strftime
 
 
 class AliesaProvider(AliBaseProvider):
@@ -15,7 +16,7 @@ class AliesaProvider(AliBaseProvider):
     endpoint = "https://esa.cn-hangzhou.aliyuncs.com"
     api_version = "2024-09-10"  # ESA API版本
     content_type = TYPE_JSON
-    remark = "Managed by DDNS"  # ESA comment 不能包含符号
+    remark = "Managed by DDNS %s" % strftime("%Y-%m-%d %H:%M:%S")
 
     def _query_zone_id(self, domain):
         # type: (str) -> str | None
